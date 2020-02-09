8fbbc092882297b69717955c0d6d679fdafd28d3
jacob <jafranswa@gmail.com>
2020-02-08 22:56:53 -0600

    cleaned up WebClientConfig.java

--- src/main/java/hello/WebClientConfig.java
+++ src/main/java/hello/WebClientConfig.java
@@ -5,14 +5,8 @@ import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-
-
 
 @Configuration
-//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,WebMvcAutoConfiguration.class })
 @LoadBalancerClient(name = "say-hello", configuration = SayHelloConfiguration.class)
 public class WebClientConfig {
 
